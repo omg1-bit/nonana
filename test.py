48ead3eb3acfa8da0429acd429c185bcc9e72214
omg1-bit <cimope5734@davopa.com>
2026-08-04 13:54:50 +0200

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import requests
-BOT_TOKEN = "8865223524:AAF-iaw7xF614IrEi1kxIYntSfPaccpN6-4"
-CHAT_ID = "8130482893"
-msg = "✅ البوت شغال وجاهز"
-url = f"https://api.telegram.org/bot8865223524:AAF-iaw7xF614IrEi1kxIYntSfPaccpN6-4/sendMessage"
-requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
